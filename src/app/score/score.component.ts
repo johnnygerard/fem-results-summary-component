38dd5a757c2adc6f5c2c47298302a8ed01fbcc2f
Johnny Gérard <john@jgerard.dev>
2023-06-16 16:40:18 +0200

    feat: summary component (#4)

--- src/app/score/score.component.ts
+++ src/app/score/score.component.ts
@@ -1,17 +1,27 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlashSvgComponent } from '../svg/flash-svg.component';
+import { BrainSvgComponent } from '../svg/brain-svg.component';
+import { ChatSvgComponent } from '../svg/chat-svg.component';
+import { EyeSvgComponent } from '../svg/eye-svg.component';
 
 @Component({
   selector: 'app-score',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FlashSvgComponent,
+    BrainSvgComponent,
+    ChatSvgComponent,
+    EyeSvgComponent,
+  ],
   templateUrl: './score.component.html',
   styleUrls: ['./score.component.scss']
 })
 export class ScoreComponent implements OnInit {
   @Input() appCategory = '';
   @Input() appScore = 0;
-  
+
   ngOnInit(): void {
     if (this.appScore < 0
       || this.appScore > 100
